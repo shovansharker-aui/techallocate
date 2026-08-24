55c453bbaf9aeb39f5b2740c86c0f741c5361b03
Ahshan-Ul Islam <mail.shovansharker@gmail.com>
2026-08-24 14:46:00 +0600

    Add CSV

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.techallocate.techallocate"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
